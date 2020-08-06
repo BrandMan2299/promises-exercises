b38b3b411f052d55c6822fe97af772322504bb4e
itai brand <67857476+BrandMan2299@users.noreply.github.com>
2020-08-06 16:09:13 +0300

    05

--- 05-callbacks/answers.js
+++ 05-callbacks/answers.js
@@ -37,7 +37,12 @@ function passwordCheckerCb(email, password, cb){
 function passwordCheckerPrms(email, password){
   return new Promise((resolve, reject) => {
     passwordCheckerCb(email, password, (error, user) => {
-      /* IMPLEMENT ME! */
+      if(!error){
+        resolve(user);
+      }
+      else{
+        reject(error);
+      }
     });
   });
 }
@@ -51,6 +56,17 @@ function passwordCheckerPrms(email, password){
  * @return {Promise<any, any>}
  */
 function makePromiseFromFunctionWithCallback(fn, ...fnParams){
+  return new Promise((resolve, reject) =>
+  {
+    fn(...fnParams, (error, value)=>{
+      if(!error){
+        resolve(value);
+      }
+      else{
+        reject(error);
+      }
+    })
+  })
   /* 
   Return a promise that 
     - calls fn with the fnParams and a callback (like fn(...fnParams, cb))
